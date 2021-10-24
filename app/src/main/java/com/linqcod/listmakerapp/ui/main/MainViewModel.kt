ea950e9cb478a3d6117602610a06017f10e4e516
linqcod <linqcod@yandex.ru>
2021-10-25 00:01:57 +0300

    view-model-added

--- app/src/main/java/com/linqcod/listmakerapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/linqcod/listmakerapp/ui/main/MainViewModel.kt
@@ -1,7 +1,35 @@
 package com.linqcod.listmakerapp.ui.main
 
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
+import com.linqcod.listmakerapp.ui.main.model.TaskList
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(
+    private val sharedPreferences: SharedPreferences
+) : ViewModel() {
+
+    lateinit var onListAdded: (() -> Unit)
+
+    val lists: MutableList<TaskList> by lazy {
+        retrieveLists()
+    }
+
+    private fun retrieveLists(): MutableList<TaskList> {
+        val sharedPreferencesContents = sharedPreferences.all
+        val taskLists = ArrayList<TaskList>()
+
+        for(taskList in sharedPreferencesContents) {
+            val itemsHashSet = ArrayList(taskList.value as HashSet<String>)
+            val list = TaskList(taskList.key, itemsHashSet)
+            taskLists.add(list)
+        }
+
+        return taskLists
+    }
+
+    fun saveList(list: TaskList) {
+        sharedPreferences.edit().putStringSet(list.title, list.tasks.toHashSet()).apply()
+        lists.add(list)
+        onListAdded.invoke()
+    }
 }
